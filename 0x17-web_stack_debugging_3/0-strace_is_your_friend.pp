13858618836b8d8c13862765961923ed31b47025
Aya Lamri <ayalamrioui19@gmail.com>
2024-08-15 00:54:52 +0000

    edcgtf

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,13 @@
-# This Puppet manifest fixes the Apache 500 error by ensuring correct permissions and restoring a missing configuration file
+# This Puppet manifest fixes the Apache 500 error by enabling the PHP5 module and ensuring Apache is running
 
-# Ensure the WordPress configuration file exists
-file { '/var/www/html/wp-config.php':
-  ensure  => file,
-  source  => 'puppet:///modules/wordpress/wp-config.php',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
+# Ensure PHP5 is installed
+package { 'php5':
+  ensure => installed,
+}
+
+# Enable the PHP5 module in Apache
+exec { 'enable_php_module':
+  command => '/usr/sbin/a2enmod php5',
+  unless  => '/usr/sbin/apache2ctl -M | grep php5_module',
+  require => Package['php5'],
 }
